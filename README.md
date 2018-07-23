354a80bc07aa4de4dece324dd25ab3e6cf60713e
Rohan Paul <rohanpaul2@gmail.com>
2018-07-23 18:13:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Boilerplate for an absolute minimum setup of a React project with webpack only
+
+## To launch in local machine
+
+```
+npm install
+npm run dist
+
+```
+
+Then open ``open dist/index.html`` in a browser
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const path = require('path');
+const config = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [                            
+                            'react',
+                            'es2015'                            
+                        ],
+                    },
+                },
+            }
+        ]
+    }
+};
+module.exports = config;
